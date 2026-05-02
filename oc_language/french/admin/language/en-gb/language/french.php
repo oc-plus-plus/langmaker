c9f3cfdb184158754aad97e5704cf45621ef69db
Oleg Kasyanov <oleg@batumi.org>
2026-05-02 20:31:15 +0400

    Initial commit

---
+++ oc_language/french/admin/language/en-gb/language/french.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']    = 'French Language';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified French language!';
+$_['text_edit']        = 'Edit French Language';
+
+// Entry
+$_['entry_status']     = 'Status';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify French language!';
