42befdea9f13532e694af39a907169ceac5cdeab
Oleg Kasyanov <oleg@batumi.org>
2026-05-04 00:53:37 +0400

    Added Russian language

---
+++ languages/russian/config.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+	// Only Latin letters and numbers are allowed in this section
+	'settings' => [
+		'name'   => 'Russian',
+		'code'   => 'ru-ru',
+		'locale' => 'ru_RU.UTF-8,ru_RU,russian,ru'
+	],
+
+	'install' => [
+		'name'        => 'Русский язык',
+		'description' => "Русская локализация для OpenCart",
+		'version'     => '1.0',
+		'author'      => 'opencart-russia',
+		'link'        => 'http://opencart-russia.ru/'
+	]
+];
