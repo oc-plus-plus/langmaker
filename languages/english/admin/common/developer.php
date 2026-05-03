168d5eb481e9d29cd61a43c097e3f25d270a144f
Oleg Kasyanov <oleg@batumi.org>
2026-05-03 23:49:59 +0400

    Added English language

---
+++ languages/english/admin/common/developer.php
@@ -0,0 +1,22 @@
+<?php
+// Heading
+$_['heading_title']          = 'Developer Options';
+
+// Text
+$_['text_developer_success'] = 'Success: You have modified developer settings!';
+$_['text_systemcache_success'] = 'Success: You have cleared the system cache!';
+$_['text_imagecache_success']  = 'Success: You have cleared the image cache!';
+$_['text_sass_success']      = 'Success: You have cleared the SASS cache!';
+$_['text_imagecache']        = 'Image Cache';
+$_['text_sass']              = 'SASS';
+$_['text_systemcache']       = 'System Cache';
+
+// Column
+$_['column_component']       = 'Component';
+$_['column_action']          = 'Action';
+
+// Entry
+$_['entry_cache']            = 'Cache';
+
+// Error
+$_['error_permission']       = 'Warning: You do not have permission to modify developer settings!';
---
+++ languages/english/config.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+	'settings' => [
+		'name'   => 'English',
+		'code'   => 'en-gb',
+		'locale' => 'en-gb,en'
+	],
+
+	'install' => [
+		'name'        => 'English',
+		'description' => "English localization of OpenCart",
+		'version'     => '1.0',
+		'author'      => 'OpenCart Community',
+		'link'        => 'https://www.opencart.com'
+	]
+];
